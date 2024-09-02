0d6a13bf1090c30a8cc4d829b59dd5e4bad7f9fb
smartrector <smartrector@gmail.com>
2024-09-02 15:59:46 +0900

    vue 01

---
+++ 02javascript/03import/person.js
@@ -0,0 +1,17 @@
+const name = ["hong", "lee"];
+const age = [10, 22];
+
+const data = [
+  {
+    id: 1,
+    title: "title",
+    content: "content",
+  },
+  {
+    id: 2,
+    title: "title2",
+    content: "content2",
+  },
+];
+
+export { name, age };
---
+++ 03vue/vuetest/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+   <div class="test">
+      <h1>title</h1>
+   </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+$color: #ddd;
+.test {
+   h1 {
+      color: red;
+   }
+   &.on {
+      background: skyblue;
+   }
+   &:hover {
+      background: $color;
+   }
+}
+</style>

d8ecbed9e5ae5aa73c85f2648f2b0b138dd311f1
smartrector <smartrector@gmail.com>
2024-09-02 16:50:39 +0900

    vue#2

--- 03vue/vuetest/src/App.vue
+++ 03vue/vuetest/src/App.vue
@@ -1,24 +1,37 @@
 <template>
-   <div class="test">
-      <h1>title</h1>
+   <div class="container secWrap">
+      <img src="./assets/images/handsome.jpg" alt="" />
+      <div class="card">
+         <h3>{{ title }}</h3>
+         <p>❤️ {{ num }}</p>
+         <p>대여 : {{ won }}</p>
+      </div>
+
+      <div v-for="(item, index) in todos" v-bind:key="index">{{ item }}</div>
+
+      <ul>
+         <li v-for="(item, i) in todos" :key="i">{{ item }}</li>
+      </ul>
    </div>
 </template>
 
 <script>
-export default {};
+export default {
+   name: 'App',
+   data() {
+      return {
+         title: '핸섬가이즈',
+         num: '8.6',
+         won: '11,000',
+         todos: ['서울', '부산', '대구'],
+      };
+   },
+};
 </script>
 
 <style lang="scss" scoped>
-$color: #ddd;
-.test {
-   h1 {
-      color: red;
-   }
-   &.on {
-      background: skyblue;
-   }
-   &:hover {
-      background: $color;
-   }
+@import './assets/css/style.scss';
+.secWrap {
+   padding: 5px 16px;
 }
 </style>

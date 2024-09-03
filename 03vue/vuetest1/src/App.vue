84e0d1e474b128755fc6a2011958363e3cc5c001
smartrector <smartrector@gmail.com>
2024-09-03 12:41:32 +0900

    vue@2

--- 03vue/vuetest1/src/App.vue
+++ 03vue/vuetest1/src/App.vue
@@ -1,18 +1,18 @@
 <template>
    <div class="container">
-      <h2>HOT & NEW</h2>
+      <h2 class="mb-10">HOT & NEW</h2>
       <div class="cardWrap">
          <div class="card" v-for="(item, i) in data" v-bind:key="i">
             <div class="imgWrap">
                <img :src="`./images/${item.img}`" :alt="`${item.img}`" />
+               <div class="hot" v-if="item.hoticon">HOT</div>
             </div>
             <div class="cardBody">
                <h4>{{ item.title }}</h4>
                <p>❤️ {{ item.num }}</p>
                <p>대여 {{ item.won }}</p>
-               <p>👍 {{ item.like }} <button>클릭</button></p>
+               <p>👍 {{ item.like }} <button v-on:click="increseLike(i)">클릭</button></p>
             </div>
-            <div class="hot">HOT</div>
          </div>
       </div>
    </div>
@@ -28,19 +28,74 @@ export default {
          data: mdata,
       };
    },
+   methods: {
+      increseLike(i) {
+         console.log(i);
+         this.data[i].like += 1;
+      },
+   },
 };
 </script>
 
 <style lang="scss" scoped>
+$radius: 5px;
 .container {
    width: 1000px;
    margin: 0 auto;
 
    @media screen and (max-width: 790px) {
       width: 100%;
+      padding: 0 16px;
    }
    img {
       display: block;
    }
+}
+
+.cardWrap {
+   display: flex;
+   flex-wrap: wrap;
+   gap: 10px;
+   .card {
+      width: calc((100% - 40px) / 5);
+      // width: 25%;
+
+      @media screen and (max-width: 790px) {
+         width: calc((100% - 10px) / 2);
+      }
+
+      @media screen and (max-width: 560px) {
+         width: 100%;
+      }
+
+      .imgWrap {
+         width: 100%;
+         position: relative;
+         overflow: hidden;
+         cursor: pointer;
+         border-radius: $radius;
+         img {
+            width: 100%;
+            transition: 0.3s;
+         }
+
+         &:hover img {
+            transform: scale(1.1);
+         }
+         .hot {
+            position: absolute;
+            top: 10px;
+            left: 10px;
+            background-color: skyblue;
+            padding: 2px 10px;
+            border-radius: 5px;
+            font-weight: bold;
+            color: white;
+         }
+      }
+   }
+}
+.mb-10 {
+   margin-bottom: 10px !important;
 }
 </style>
--- 03vue/vuetest1/src/assets/mdata.js
+++ 03vue/vuetest1/src/assets/mdata.js
@@ -1,10 +1,10 @@
 const mdata = [
    { title: '핸섬가이즈', num: '8.6', won: '11,000', img: 'handsome.jpg', like: 0, hoticon: true },
-   { title: '탈주', num: '8.1', won: '11,000', img: 'movie.jpg', like: 0, hoticon: true },
+   { title: '탈주', num: '8.1', won: '11,000', img: 'movie.jpg', like: 0, hoticon: false },
    { title: '하이재킹', num: '8.7', won: '10,900', img: 'movie1.jpg', like: 0, hoticon: true },
-   { title: '핸섬가이즈', num: '8.6', won: '11,000', img: 'handsome.jpg', like: 0, hoticon: true },
-   { title: '탈주', num: '8.1', won: '11,000', img: 'movie.jpg', like: 0, hoticon: true },
-   { title: '하이재킹', num: '8.7', won: '10,900', img: 'movie1.jpg', like: 0, hoticon: true },
+   { title: '핸섬가이즈', num: '8.6', won: '11,000', img: 'handsome.jpg', like: 0, hoticon: false },
+   { title: '탈주', num: '8.1', won: '11,000', img: 'movie.jpg', like: 0, hoticon: false },
+   { title: '하이재킹', num: '8.7', won: '10,900', img: 'movie1.jpg', like: 0, hoticon: false },
 ];
 
 export default mdata;

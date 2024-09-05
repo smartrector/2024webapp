b611482f4137fd127c4b9952412d7e1ff4dc2a90
smartrector <smartrector@gmail.com>
2024-09-05 14:54:15 +0900

    vue#router#2

---
+++ 03vue/vueproject1/src/components/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="container-fluid d-flex justify-content-center align-items-center"
+    style="height: 100%"
+    id="home"
+  >
+    <div class="container">
+      <div class="row">
+        <div class="col-md-5 d-flex align-items-center order-2 order-md-1">
+          <div class="text-center text-md-start">
+            <h3>Lorem, ipsum dolor.</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Accusantium, molestias?
+            </p>
+            <div class="btn btn-primary">Get it On Chrome</div>
+          </div>
+        </div>
+        <div class="col-md-7 order-1 order-md-2">
+          <img
+            src="../assets/images/illustration-hero.svg"
+            alt=""
+            class="img-fluid"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ 03vue/vueproject1/src/components/NavbarView.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="navbar navbar-expand-md bg-body-tertiary fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img
+          src="../assets/images/logo_innogrid.svg"
+          alt=""
+          style="width: 120px"
+        />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="#home">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">ABOUT</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">PORTFOLIO</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>

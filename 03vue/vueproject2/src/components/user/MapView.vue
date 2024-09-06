bb7ec05b6b659dad4a6a760b315a2f13c61a413b
smartrector <smartrector@gmail.com>
2024-09-06 14:59:09 +0900

    map

--- 03vue/vueproject2/src/components/user/MapView.vue
+++ 03vue/vueproject2/src/components/user/MapView.vue
@@ -1,20 +1,76 @@
 <template>
-  <div>지도</div>
+  <div>지도 {{ moveLat }} / {{ moveLng }}</div>
   <div id="map" style="width: 500px; height: 400px"></div>
+
+  {{ latitude }}{{ longitude }}
 </template>
 
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
+
+const latitude = ref(0);
+const longitude = ref(0);
+
+const moveLat = ref(0);
+const moveLng = ref(0);
 
 onMounted(() => {
-  var container = document.getElementById("map");
-  var options = {
-    center: new kakao.maps.LatLng(33.450701, 126.570667),
+  if (!("geolocation" in navigator)) {
+    return;
+  }
+
+  navigator.geolocation.getCurrentPosition(
+    (pos) => {
+      console.log(pos.coords.latitude, pos.coords.longitude);
+
+      latitude.value = pos.coords.latitude;
+      longitude.value = pos.coords.longitude;
+
+      //   initMap();
+
+      if (window.kakao && window.kakao.maps) {
+        initMap();
+      } else {
+        const script = document.createElement("script");
+        /* global kakao */
+        script.onload = () => kakao.maps.load(initMap);
+        script.src =
+          "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=05222ace53571c8fbb636c91def0fbc2";
+        document.head.appendChild(script);
+      }
+    },
+    (err) => {
+      alert(err.message);
+    }
+  );
+});
+
+const initMap = () => {
+  const container = document.getElementById("map");
+  let options = {
+    center: new kakao.maps.LatLng(latitude.value, longitude.value),
     level: 3,
   };
 
-  var map = new kakao.maps.Map(container, options);
-});
+  let map = new kakao.maps.Map(container, options);
+
+  var markerPosition = new kakao.maps.LatLng(latitude.value, longitude.value);
+  // 마커를 생성합니다
+  var marker = new kakao.maps.Marker({
+    position: markerPosition,
+  });
+
+  // 마커가 지도 위에 표시되도록 설정합니다
+  marker.setMap(map);
+
+  //지도 중심좌표
+  kakao.maps.event.addListener(map, "center_changed", function () {
+    var latlng = map.getCenter();
+
+    moveLat.value = latlng.getLat();
+    moveLng.value = latlng.getLng();
+  });
+};
 </script>
 
 <style lang="scss" scoped></style>

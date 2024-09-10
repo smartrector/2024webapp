bedfded49630d0a7134b59c6d8bc57d333f55506
smartrector <smartrector@gmail.com>
2024-09-10 12:30:38 +0900

    react#2

--- 04react/reacttest1/src/App.js
+++ 04react/reacttest1/src/App.js
@@ -6,6 +6,9 @@ function App() {
   let text = "hello";
   const [box, setBox] = useState("han");
   const [num, setNum] = useState(0);
+  let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState("안녕하세요");
+  let [shop, setShop] = useState(["사과", "복숭아", "바나나"]);
 
   return (
     <>
@@ -36,10 +39,35 @@ function App() {
       >
         num
       </button>
+
+      <button
+        onClick={() => {
+          setModal(!modal);
+        }}
+      >
+        모달
+      </button>
+      {shop.map((item, i) => {
+        return (
+          <>
+            <div key={i}>{item}</div>
+          </>
+        );
+      })}
+
+      {modal == true ? <Modal title={title} image="test1" /> : null}
     </>
-    {
-      
-    }
+  );
+}
+
+function Modal(props) {
+  return (
+    <>
+      <div className="modal">
+        <h3>공지사항{props.title}</h3>
+        <p>상세내용{props.image}</p>
+      </div>
+    </>
   );
 }
 

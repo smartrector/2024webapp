dd87ba29eaaf62882b549388460c7d659aa8d428
smartrector <smartrector@gmail.com>
2024-09-20 12:40:13 +0900

    todoapi#2

--- 05tododev/front/src/components/todo/ListComp.jsx
+++ 05tododev/front/src/components/todo/ListComp.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { getList } from "../../api/todoApi";
+import { useSearchParams } from "react-router-dom";
+import useCustomMove from "../../hooks/useCustomMove";
 
 const initState = {
   dtoList: [],
@@ -15,13 +17,34 @@ const initState = {
 };
 
 function ListComp() {
+  const { page, size } = useCustomMove();
   const [data, setData] = useState(initState);
+  //   const [queryParams] = useSearchParams();
+  //   console.log(queryParams.get("size"));
+
+  //   const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1;
+  //   const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10;
+
+  //   console.log(page, size);
   useEffect(() => {
     getList({ page, size }).then((res) => {
       console.log(res);
+      setData(res);
     });
-  }, []);
-  return <div>ListComp</div>;
+  }, [page, size]);
+  return (
+    <div>
+      {data.dtoList.map((item, i) => {
+        return (
+          <div key={i} className="flex border-b-2 py-3">
+            <div className="text-2xl w-1/12">{item.tno}</div>
+            <div className="text-2xl w-7/12 font-extrabold">{item.title}</div>
+            <div className="text-2xl w-4/12">{item.dueDate}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default ListComp;
--- 05tododev/front/src/hooks/useCustomMove.jsx
+++ 05tododev/front/src/hooks/useCustomMove.jsx
@@ -1,3 +1,17 @@
-const useCustomMove = () => {};
+import { useSearchParams } from "react-router-dom";
+
+const getNum = (param, dValue) => {
+  if (!param) {
+    return dValue;
+  }
+  return parseInt(param);
+};
+
+const useCustomMove = () => {
+  const [queryParams] = useSearchParams();
+  const page = getNum(queryParams.get("page"), 1);
+  const size = getNum(queryParams.get("size"), 10);
+  return { page, size };
+};
 
 export default useCustomMove;
